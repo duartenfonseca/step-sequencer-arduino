c4197ddc8b0608620b18a5b9a5920d7020fe2958
Duarte Fonseca <duarte.n.fonseca@gmail.com>
2022-11-09 23:46:37 +0000

    Added the ability to set the timer interrupt cycle with ms

--- timer_interrup/timer_interrup.ino
+++ timer_interrup/timer_interrup.ino
@@ -1,105 +1,66 @@
-//timer setup for timer0, timer1, and timer2.
-//For arduino uno or any board with ATMEL 328/168.. diecimila, duemilanove, lilypad, nano, mini...
-
 //this code will enable all three arduino timer interrupts.
-//timer0 will interrupt at 2kHz
 //timer1 will interrupt at 1Hz
-//timer2 will interrupt at 8kHz
 
 //storage variables
 bool toggle0 = 0;
 bool toggle1 = 0;
 bool toggle2 = 0;
+long int t2 = 0;
+long int t1 = 0;
 
-void setup(){
-  
+// Get the OCR1A value from the number of milliseconds for the loop
+long msToOcr1a(int milliseconds){
+  long result;
+  result = (16*pow(10,6)*milliseconds)/((long)2000*1024)-1;
+  return result;
+}
+
+void setup() {
+
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   pinMode(13, OUTPUT);
 
-cli();//stop interrupts
+  // Baud Rate
+  Serial.begin(115200);
 
-//set timer0 interrupt at 2kHz
-  TCCR0A = 0;// set entire TCCR2A register to 0
-  TCCR0B = 0;// same for TCCR2B
-  TCNT0  = 0;//initialize counter value to 0
-  // set compare match register for 2khz increments
-  OCR0A = 124;// = (16*10^6) / (2000*64) - 1 (must be <256)
-  // turn on CTC mode
-  TCCR0A |= (1 << WGM01);
-  // Set CS01 and CS00 bits for 64 prescaler
-  TCCR0B |= (1 << CS01) | (1 << CS00);   
-  // enable timer compare interrupt
-  TIMSK0 |= (1 << OCIE0A);
+  // Auxiliar variable to check timing of interrupt
+  long int t1 = millis();
 
-//set timer1 interrupt at 1Hz
+  cli();  //stop interrupts
   TCCR1A = 0;// set entire TCCR1A register to 0
   TCCR1B = 0;// same for TCCR1B
   TCNT1  = 0;//initialize counter value to 0
   // set compare match register for 1hz increments
-  OCR1A = 31249;// = (16*10^6) / (0.5*1024) - 1 (must be <65536)
+  OCR1A = msToOcr1a(10); // set the milliseconds for the timer
   // turn on CTC mode
   TCCR1B |= (1 << WGM12);
-  // Set CS12 and CS10 bits for 1024 prescaler
+  // Set CS10 and CS12 bits for 1024 prescaler
   TCCR1B |= (1 << CS12) | (1 << CS10);  
   // enable timer compare interrupt
   TIMSK1 |= (1 << OCIE1A);
 
-//set timer2 interrupt at 8kHz
-  TCCR2A = 0;// set entire TCCR2A register to 0
-  TCCR2B = 0;// same for TCCR2B
-  TCNT2  = 0;//initialize counter value to 0
-  // set compare match register for 8khz increments
-  OCR2A = 249;// = (16*10^6) / (8000*8) - 1 (must be <256)
-  // turn on CTC mode
-  TCCR2A |= (1 << WGM21);
-  // Set CS21 bit for 8 prescaler
-  TCCR2B |= (1 << CS21);   
-  // enable timer compare interrupt
-  TIMSK2 |= (1 << OCIE2A);
+  sei();  //allow interrupts
 
+}  //end setup
 
-sei();//allow interrupts
-
-}//end setup
-
-ISR(TIMER0_COMPA_vect){//timer0 interrupt 2kHz toggles pin 8
-//generates pulse wave of frequency 2kHz/2 = 1kHz (takes two cycles for full wave- toggle high then toggle low)
-  if (toggle0){
-    digitalWrite(8,HIGH);
-    toggle0 = 0;
-  }
-  else{
-    digitalWrite(8,LOW);
-    toggle0 = 1;
-  }
-}
-
-ISR(TIMER1_COMPA_vect){//timer1 interrupt 1Hz toggles pin 13 (LED)
-//generates pulse wave of frequency 1Hz/2 = 0.5kHz (takes two cycles for full wave- toggle high then toggle low)
-  if (toggle1){
-    digitalWrite(13,HIGH);
+ISR(TIMER1_COMPA_vect) {  //timer1 interrupt 1Hz toggles pin 13 (LED)
+                          //generates pulse wave of frequency 1Hz/2 = 0.5kHz (takes two cycles for full wave- toggle high then toggle low)
+  if (toggle1) {
+    digitalWrite(13, HIGH);
+    t2 = millis();
+    Serial.print("high: ");
+    Serial.print(t2 - t1);
+    t1 = t2;
+    Serial.println(" milliseconds");
     toggle1 = 0;
   }
-  else{
-    digitalWrite(13,LOW);
+
+  else {
+    digitalWrite(13, LOW);
     toggle1 = 1;
   }
 }
-  
-ISR(TIMER2_COMPA_vect){//timer1 interrupt 8kHz toggles pin 9
-//generates pulse wave of frequency 8kHz/2 = 4kHz (takes two cycles for full wave- toggle high then toggle low)
-  if (toggle2){
-    digitalWrite(9,HIGH);
-    toggle2 = 0;
-  }
-  else{
-    digitalWrite(9,LOW);
-    toggle2 = 1;
-  }
-}
 
-
-void loop(){
-  //do other things here
+void loop() {
 }
